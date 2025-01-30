96d3b32806a6cbf2f700d5ef632cfc9456910959
gilmerperez <gilmer2002@outlook.com>
2025-01-30 14:41:59 -0500

    Added tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,15 @@ CREATE DATABASE movies_db;
 \c movies_db;
 
 /*//TODO: Create a movie table */
-
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    movie_name VARCHAR(100) NOT NULL
+);
 
 /*//TODO: Create a review table */
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    movie_id INT,
+    review TEXT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE SET NULL
+);
