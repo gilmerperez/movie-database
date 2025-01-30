07064e15fb5e2befaa08ec4b96b076d62860e24a
gilmerperez <gilmer2002@outlook.com>
2025-01-30 14:46:20 -0500

    Added join clauses in query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,6 @@
-/*//* You can practice your join queries here */
+-- You can practice your join queries here
+SELECT movies.movie_name AS movie, reviews.review
+FROM reviews
+LEFT JOIN movies
+ON reviews.movie_id = movies.id
+ORDER BY movies.movie_name;
